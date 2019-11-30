ff35894869f1302e47260bf2df02fd27ece24d9f
Rodion Konioshko <rudykonio@gmail.com>
2019-11-30 18:27:08 +0200

    meme part 1

--- MeMe/ViewController.swift
+++ MeMe/ViewController.swift
@@ -8,14 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate,UITextFieldDelegate {
     @IBOutlet weak var cameraBarButtonItem: UIBarButtonItem!
     @IBOutlet weak var memeImageView: UIImageView!
+    @IBOutlet weak var topToolBar: UIToolbar!
+    @IBOutlet weak var botToolBar: UIToolbar!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var botTextField: UITextField!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    var isFirstKeyboardAppearence:Bool = false
     var uiImagePickerController : UIImagePickerController!
+    let top = "TOP"
+    let bottom = "BOTTOM"
+    let emptyString = ""
+    let topTextFieldId = "top_text_field"
+    let botTextFieldId = "bot_text_field"
+    var isTopTextField = false
     override func viewDidLoad() {
         super.viewDidLoad()
         handleCameraButtonOnLoad()
         initUiImagePickerController()
+        configureTextAppearance()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        subscribeToKeyboarNotifications()
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeToKeyboarNotifications()
     }
     @IBAction func albumAction(_ sender: Any) {
         uiImagePickerController.sourceType = .photoLibrary
@@ -24,6 +45,14 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
     @IBAction func cameraAction(_ sender: Any) {
         uiImagePickerController.sourceType = .camera
         present(uiImagePickerController, animated: true, completion: nil)
+    }
+    @IBAction func cancelAction(_ sender: Any) {
+        topTextField.text = top
+        botTextField.text = bottom
+        memeImageView.image = nil
+    }
+    @IBAction func shareAction(_ sender: Any) {
+        presentActivityVC()
     }
     func isCameraAvailable() -> Bool{
         return UIImagePickerController.isSourceTypeAvailable(.camera)
@@ -38,6 +67,7 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
             let image = info[.originalImage] as? UIImage
             if let image = image{
                 memeImageView.image = image
+                shareButton.isEnabled = true
             }
         }
         dismiss(animated: true, completion: nil)
@@ -54,6 +84,113 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
         uiImagePickerController = UIImagePickerController()
         uiImagePickerController.delegate = self
     }
+    func configureTextAppearance(){
+        let memeTextAttributes: [NSAttributedString.Key: Any] = [
+            NSAttributedString.Key.strokeColor: UIColor.black,
+            NSAttributedString.Key.foregroundColor: UIColor.white,
+            NSAttributedString.Key.strokeWidth: -1,
+            NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!
+        ]
+        let textfieldArr = [topTextField,botTextField]
+        for textField in textfieldArr{
+            textField?.defaultTextAttributes = memeTextAttributes
+            textField?.textAlignment = .center
+            textField?.backgroundColor = .clear
+            textField?.borderStyle = .none
+            textField?.delegate = self
+        }
+    }
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if (textField.accessibilityIdentifier == topTextFieldId){
+            isTopTextField = true
+            if(textField.text == top){
+                clearText(uiTextField: textField)
+            }
+        }
+        else if(textField.accessibilityIdentifier == botTextFieldId){
+            if(textField.text == bottom){
+             clearText(uiTextField: textField)
+            }
+        }
+    }
+    func clearText(uiTextField:UITextField){
+        uiTextField.text = emptyString
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if(textField.text == emptyString){
+            switch textField.accessibilityIdentifier {
+            case topTextFieldId:
+                textField.text = top
+            case botTextFieldId:
+                textField.text = bottom
+            default:
+                break
+            }
+        }
+        if (textField.accessibilityIdentifier == topTextFieldId){
+            isTopTextField = false
+        }
+    }
+    func isLandscape() ->Bool{
+        if let orientation = UIApplication.shared.windows.first?.windowScene?.interfaceOrientation{
+            if(orientation.isLandscape){
+                return true
+            }
+        }
+        return false
+    }
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+           /**
+            First keyboard appearnce lifts the keyboard with bot toolbar, it doesn't look good, that's a small hack in order to make it prettier.
+            */
+            if(!isFirstKeyboardAppearence && !isTopTextField){
+            isFirstKeyboardAppearence = true
+            if(isLandscape()){
+                    view.frame.origin.y -= (keyboardSize.height - 50.0)
+                }else{
+                    view.frame.origin.y -= (keyboardSize.height - 45.0)
+                }
+            }
+        }
+    }
+    @objc func keyboardWillHide(notification: NSNotification) {
+            view.frame.origin.y = 0
+            isFirstKeyboardAppearence = false
+    }
+    func subscribeToKeyboarNotifications(){
+            NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+            NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        }
+    func unsubscribeToKeyboarNotifications(){
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return textField.endEditing(false)
+    }
+    func generateMemedImage() -> UIImage {
+        topToolBar.isHidden = true
+        botToolBar.isHidden = true
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let memedImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        topToolBar.isHidden = false
+        botToolBar.isHidden = false
+        return memedImage
+    }
+    func presentActivityVC() {
+        let memedImage = generateMemedImage()
+        let activityVC = UIActivityViewController(activityItems: [memedImage], applicationActivities: nil)
+        activityVC.completionWithItemsHandler = {activity,success,returnedItems,error in
+            if(success){
+                self.save(memedImage: memedImage)
+            }
+        }
+        present(activityVC, animated: true, completion: nil)
+    }
+    func save(memedImage:UIImage){
+    let meme = MemeModel(topText: topTextField.text!, botText: botTextField.text!, image: memeImageView.image!, memedImage:memedImage)
+    }
 }
-
-
---
+++ MeMe/model/MemeModel.swift
@@ -0,0 +1,16 @@
+//
+//  MemeModel.swift
+//  MeMe
+//
+//  Created by Rodion Konioshko on 30/11/2019.
+//  Copyright © 2019 Rodion Konioshko. All rights reserved.
+//
+
+import Foundation
+import UIKit
+struct MemeModel {
+    let topText:String
+    let botText:String
+    let image:UIImage
+    let memedImage:UIImage
+}
